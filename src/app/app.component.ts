7a84b34c360d1b28173b5a2466de055d86f64dbe
Juan Fernando Berrazueta <juanfersubia@gmail.com>
2022-01-08 23:09:38 -0500

    Finish Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,11 @@ import { DataService } from './services/data.service';
 import { Componentes } from 'src/assets/interfaces/interfaces';
 import { Observable } from 'rxjs/internal/Observable';
 
+interface Menu{
+  direccion: string;
+  nombre: string;
+  icono: any;
+}
 
 @Component({
   selector: 'app-root',
@@ -11,6 +16,33 @@ import { Observable } from 'rxjs/internal/Observable';
 })
 export class AppComponent {
 
+  menu: Menu[]= [
+    {
+      direccion: '',
+      nombre: 'Home',
+      icono: 'home-outline',
+    },
+    {
+      direccion: '',
+      nombre: 'Sculptures',
+      icono: 'body-outline',
+    },
+    {
+      direccion: '',
+      nombre: 'Paintings',
+      icono: 'brush-outline',
+    },
+    {
+      direccion: '',
+      nombre: 'Top Artists',
+      icono: 'trophy-outline',
+    },
+    {
+      direccion: '',
+      nombre: 'How to Art?',
+      icono: 'help-circle-outline',
+    },
+  ]
   
   constructor(private dataService: DataService) {}
 
