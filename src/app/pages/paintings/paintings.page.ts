e0fc9fe68d5262d9156f8f9012ad78ae63b31a70
Juan Fernando Berrazueta <juanfersubia@gmail.com>
2022-01-24 11:52:42 -0500

    paintings

--- src/app/pages/paintings/paintings.page.ts
+++ src/app/pages/paintings/paintings.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface Paintings {
+  direccion: string;
+  nombre: string;
+  imagen: any;
+  precio: any;
+}
 
 @Component({
   selector: 'app-paintings',
@@ -7,9 +15,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaintingsPage implements OnInit {
 
-  constructor() { }
+  paintings: Paintings[]=[
+    {
+      direccion: '/sculptures',
+     nombre: 'MantaRaya',
+     imagen: 'assets/cuadrado.jpeg',
+     precio: '3000'
+    },
+    {
+     direccion: '/paintings',
+    nombre: 'VidaSolar',
+    imagen: 'assets/cuadrado.jpeg',
+    precio: '3000'
+    
+   },{
+     direccion: '/others',
+    nombre: 'SaltoAlto',
+    imagen: 'assets/cuadrado.jpeg',
+    precio: '3000'
+   },
+  ]
+
+  constructor(private route: Router) { }
 
   ngOnInit() {
   }
-
+  onClickHome(){
+    this.route.navigate(['/home']);
+  }
 }
