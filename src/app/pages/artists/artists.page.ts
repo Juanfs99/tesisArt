8cdb459ffb195c3ae97dd293226ebd4008864a82
Juan Fernando Berrazueta <juanfersubia@gmail.com>
2022-01-24 12:49:04 -0500

    .

--- src/app/pages/artists/artists.page.ts
+++ src/app/pages/artists/artists.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-artists',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistsPage implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
+  }
+  onClickHome(){
+    this.route.navigate(['/home']);
   }
 
 }
--- src/app/pages/how-art/how-art.page.ts
+++ src/app/pages/how-art/how-art.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-how-art',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HowArtPage implements OnInit {
 
-  constructor() { }
+  
+  constructor(private route: Router) { }
 
   ngOnInit() {
+  }
+  onClickHome(){
+    this.route.navigate(['/home']);
   }
 
 }
