eeee2d1a31caab033f0daadcf9846c37b8070dbc
Juan Fernando Berrazueta <juanfersubia@gmail.com>
2022-02-01 14:40:13 -0500

    mmm

--- src/app/pages/artistainfo/artistainfo.page.ts
+++ src/app/pages/artistainfo/artistainfo.page.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {Location} from '@angular/common';
 
+interface Sculptures {
+  direccion: string;
+  nombre: string;
+  imagen: any;
+  precio: any;
+}
 @Component({
   selector: 'app-artistainfo',
   templateUrl: './artistainfo.page.html',
@@ -7,9 +15,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistainfoPage implements OnInit {
 
-  constructor() { }
+
+  sculptures: Sculptures[]=[
+    {
+      direccion: '',
+     nombre: 'MantaRaya',
+     imagen: 'assets/cuadrado.jpeg',
+     precio: '3000'
+    },
+    {
+     direccion: '',
+    nombre: 'VidaSolar',
+    imagen: 'assets/cuadrado.jpeg',
+    precio: '3000'
+    
+   },{
+     direccion: '',
+    nombre: 'SaltoAlto',
+    imagen: 'assets/cuadrado.jpeg',
+    precio: '3000'
+   },
+  ]
+
+  constructor(private route: Router, private _location : Location) { }
 
   ngOnInit() {
   }
 
+  onClickBack(){
+    this._location.back();
+
+  }
 }
