0106e6d6a8e75d090fc3581b256cbc7b1f31ba6c
Juanfer <juanfersubia@gmail.com>
2022-02-03 08:32:14 -0500

    fixed ts articulo

--- src/app/pages/articulo/articulo.page.ts
+++ src/app/pages/articulo/articulo.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Location} from '@angular/common';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-articulo',
@@ -7,22 +7,22 @@ import {Location} from '@angular/common';
   styleUrls: ['./articulo.page.scss'],
 })
 export class ArticuloPage implements OnInit {
-
-  constructor( private _location : Location) { }
-
-  ngOnInit() {
-  }
-  onClickBack(){
-    this._location.back();
-
-
-  }
   option = {
     slidesPerView: 1.5,
     centeredSlides: true,
     loop: true,
     spaceBetween: 10,
     // autoplay:true,
+  };
+  constructor(private _location: Location) { }
+
+  ngOnInit() {
   }
+  onClickBack() {
+    this._location.back();
+
+
+  }
+
 }
 
