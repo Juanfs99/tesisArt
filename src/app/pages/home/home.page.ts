0fda35b0a09de9ea4ecd366bad764041a923318d
Juanfer <juanfersubia@gmail.com>
2022-03-21 23:08:18 -0500

    adding photos to categories

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,11 @@
 import { ScreensizeService } from './../../services/screensize.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {
+  Auth, signOut,
+  authState
+} from '@angular/fire/auth';
+import { AvatarService } from '../../services/avatar.service';
 
 interface Categorias {
   direccion: string;
@@ -23,18 +28,18 @@ export class HomePage implements OnInit {
   categorias: Categorias[] = [
     {
       direccion: '/sculptures',
-      nombre: 'Sculptures',
-      imagen: 'assets/cuadrado.jpeg',
+      nombre: 'Esculturas',
+      imagen: 'https://firebasestorage.googleapis.com/v0/b/art-in-augmented-reality.appspot.com/o/AppComponentes%2FImagenes%2FsculptureCat.jpg?alt=media&token=f00b3792-a957-4214-8982-124b326cfe4d',
     },
     {
       direccion: '/paintings',
-      nombre: 'Paintings',
-      imagen: 'assets/cuadrado.jpeg',
+      nombre: 'Pinturas',
+      imagen: 'https://firebasestorage.googleapis.com/v0/b/art-in-augmented-reality.appspot.com/o/AppComponentes%2FImagenes%2FpinturaCat.jpg?alt=media&token=21e1cd85-45ad-4ecb-83cc-53b2e67ee129',
 
     }, {
       direccion: '/digital',
       nombre: 'Digital',
-      imagen: 'assets/cuadrado.jpeg',
+      imagen: 'https://firebasestorage.googleapis.com/v0/b/art-in-augmented-reality.appspot.com/o/AppComponentes%2FImagenes%2FdigitalCat.jpg?alt=media&token=4777931b-79d6-4ab8-9075-8c9610ec3c48',
 
     },
   ];
@@ -57,17 +62,44 @@ export class HomePage implements OnInit {
 
   ];
   isDesktop: boolean;
+  login: boolean;
 
+  profile = null;
 
   constructor(private screensizeService: ScreensizeService,
-    private route: Router,) {
+    private route: Router,
+    private authFire: Auth,
+    private avatarService: AvatarService,) {
     this.screensizeService.isDesktopView().subscribe(isDesktop => {
       console.log('IsDesktop changed', isDesktop);
       this.isDesktop = isDesktop;
     });
+    authState(this.authFire).subscribe((response) => {
+      if (response) {
+        console.log('esta logeado');
+
+
+        this.login = true;
+
+        this.avatarService.getUserProfile().subscribe((data) => {
+          this.profile = data;
+
+
+        });
+
+
+      } else {
+        console.log('no esta loguead');
+        this.login = false;
+
+      }
+    });
   }
   onClickLogin() {
     this.route.navigate(['/login']);
+  }
+  onClickProfile() {
+    this.route.navigate(['/profile']);
   }
   ngOnInit() {
   }
