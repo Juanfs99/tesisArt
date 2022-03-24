e5dee35d04b4096ed6b391e0f2d0576c2bc74d34
Juanfer <juanfersubia@gmail.com>
2022-03-23 23:16:28 -0500

    css finished upload files

--- src/app/pages/subir/subir.page.ts
+++ src/app/pages/subir/subir.page.ts
@@ -16,11 +16,7 @@ export class SubirPage implements OnInit {
   obras: FormGroup;
 
   constructor(private route: Router,
-    private loadingController: LoadingController,
-    private avatarService: AvatarService,
-    private dataService: DataService,
-    private fb: FormBuilder,
-    private alertController: AlertController,
+
     private modalCtrl: ModalController,
 
 
@@ -29,70 +25,19 @@ export class SubirPage implements OnInit {
     this.route.navigate(['/home']);
   }
 
-  async crearObra() {
-    const loading = await this.loadingController.create();
-    await loading.present();
-    this.dataService.addObra(this.obras.value)
-      .then(async (obras) => {
-        if (obras) {
-          loading.dismiss();
-          this.showAlert('Obra Registrada', 'Esta obra entrará en revisión');
 
-        } else {
-          loading.dismiss();
-          this.showAlert('Fallo de registro', 'Por favor, intente de nuevo');
-        }
-      });
-
-  }
   async abrirModalSubir() {
     const modal = await this.modalCtrl.create({
       component: ModalSubirPage,
-
+      cssClass: 'my-custom-modal-css'
 
     });
     await modal.present();
 
   }
 
-  async showAlert(header, message) {
-    const alert = await this.alertController.create({
-      header,
-      message,
-      buttons: ['OK'],
-    });
-    await alert.present();
-  }
-  get nombre() {
-    return this.obras.get('nombre');
-  }
-  get material() {
-    return this.obras.get('material');
-  }
-  get concepto() {
-    return this.obras.get('concepto');
-  }
-  get dimensiones() {
-    return this.obras.get('dimensiones');
-  }
 
-  get precio() {
-    return this.obras.get('precio');
-  }
-
-  get modeloObra() {
-    return this.obras.get('modeloObra');
-  }
   ngOnInit() {
-    this.obras = this.fb.group({
-      nombre: ['', [Validators.required]],
-      concepto: ['', [Validators.required]],
-
-      material: ['', [Validators.required]],
-      dimensiones: ['', [Validators.required]],
-      precio: ['', [Validators.required]],
-      modeloObra: ['', [Validators.required]],
-
-    });
+    ;
   }
 }
